5549ef98dc2db1692ca4973cba85b48bed364a7b
Simas Armolaitis <simaarmo@k3kl.lt>
2020-09-08 16:58:45 +0300

    excel download push with reports

--- rkl_app/src/Components/ExportingSection/ExportingSection.jsx
+++ rkl_app/src/Components/ExportingSection/ExportingSection.jsx
@@ -3,11 +3,23 @@ import "./ExportingSection.scss";
 
 const ExportingSection = ({sortBy, ConsiliumTab}) => {
     
+    const currentDate = new Date();
+
+    const defaultDate = (thisday = currentDate.getDate()) => {
+       let year = currentDate.getFullYear();
+       let month = currentDate.getMonth()+1 > 9 ? currentDate.getMonth()+1 : `0${currentDate.getMonth()+1}` ;
+       let day = thisday > 9 ? thisday : `0${thisday}`;
+       let fulldate = `${year}-${month}-${day}`;
+
+       return fulldate;
+    }
+    const defaultTime = Date().slice(16,21);
+
     const [sorting, setSorting] = useState(sortBy);
-    const [startingDate, setStartingDate] = useState(''); 
-    const [endingDate, setEndingDate] = useState('');
-    const [startingTime, setStartingTime] = useState('');
-    const [endingTime, setEndingTime] = useState('');
+    const [startingDate, setStartingDate] = useState(defaultDate(1)); 
+    const [endingDate, setEndingDate] = useState(defaultDate());
+    const [startingTime, setStartingTime] = useState('08:00');
+    const [endingTime, setEndingTime] = useState(defaultTime);
 
     const updateStartingDate = (event) => {
         const { value } = event.target;
@@ -35,15 +47,19 @@ const ExportingSection = ({sortBy, ConsiliumTab}) => {
     }
 
     const onGetConsultReport = () => {
-        fetch(`http://172.18.218.15:5001/consultation/report?sorting=${sorting}&startingDate=${startingDate}&startingTime=${startingTime}&endingDate=${endingDate}&endingTime=${endingTime}`)
+        let url = `http://172.18.218.15:5001/report?sorting=${sorting}&startingDate=${startingDate}&startingTime=${startingTime}&endingDate=${endingDate}&endingTime=${endingTime}`;
+        fetch(url)
         .then((response) => response.json())
         .catch(err => console.error(err));
+        window.open(url,'_blank');
     }
 
     const onGetConsiliumReport = () => {
-        fetch(`http://172.18.218.15:5001/consilium/report?sorting=${sorting}&startingDate=${startingDate}&startingTime=${startingTime}&endingDate=${endingDate}&endingTime=${endingTime}`)
+        let url = `http://172.18.218.15:5001/consilium/report?sorting=${sorting}&startingDate=${startingDate}&startingTime=${startingTime}&endingDate=${endingDate}&endingTime=${endingTime}`;
+        fetch(url)
         .then((response) => response.json())
         .catch(err => console.error(err));
+        window.open(url,'_blank');
     }
 
     return (
@@ -63,11 +79,11 @@ const ExportingSection = ({sortBy, ConsiliumTab}) => {
                     <option value="AcceptBy">Priemė</option>
                 </select>
                 <label htmlFor="DateFrom">Nuo: </label>
-                <input type="date" id="DateFrom" name="DateFrom" value={startingDate} onChange={updateStartingDate}/>
-                <input type="time" id="TimeFrom" name="TimeFrom" value={startingTime} onChange={updateStartingTime}/>
+                <input type="date" id="DateFrom" name="DateFrom" placeholder={defaultDate(1)} value={startingDate} onChange={updateStartingDate}/>
+                <input type="time" id="TimeFrom" name="TimeFrom" placeholder={'08:00'} value={startingTime} onChange={updateStartingTime}/>
                 <label htmlFor="DateUntil">Iki:</label>
-                <input type="date" id="DateUntil" name="DateUntil" value={endingDate} onChange={updateEndingDate}/>
-                <input type="time" id="TimeUntil" name="TimeUntil" value={endingTime} onChange={updateEndingTime}/>
+                <input type="date" id="DateUntil" name="DateUntil" placeholder={defaultDate()} value={endingDate} onChange={updateEndingDate}/>
+                <input type="time" id="TimeUntil" name="TimeUntil" placeholder={defaultTime} value={endingTime} onChange={updateEndingTime}/>
             </form>
             <button onClick={ConsiliumTab ? onGetConsiliumReport : onGetConsultReport}>Ataskaita</button>
         </div>
--- rkl_app/src/Server/server.js
+++ rkl_app/src/Server/server.js
@@ -3,7 +3,6 @@ const express = require('../node_modules/express');
 const mariadb = require('../node_modules/mariadb');
 const cors = require('../node_modules/cors');
 const excel = require('../node_modules/exceljs');
-const tempfile = require('tempfile');
 
 const app = express();
 const port = 5001;
@@ -16,7 +15,8 @@ const pool = mariadb.createPool({
     password: 'kometa',
     database: 'ligtreDB',
     connectionLimit: 10,
-    idleTimeoutMillis: 1000
+    idleTimeoutMillis: 1000,
+    multipleStatements: true
 });
 
 app.listen(port, () => {
@@ -73,42 +73,85 @@ app.get('/consilium/add', (req, res) => {
 
 /* Exporting excel */
 
-const workbook = new excel.Workbook();
-const worksheet = workbook.addWorksheet('Consultation');
+app.get('/report', (req, res) => {
 
-worksheet.columns = [
-  {header: 'Id', key: '_ID', width: 10},
-  {header: 'Data', key: 'Time', width: 20},
-  {header: 'Skyrius', key: 'Department', width: 20},
-  {header: 'Skubus ar planinis', key: 'Urgency', width: 20},
-  {header: 'Palatos nr.', key: 'Room', width: 20},
-  {header: 'Kvieciantysis gyd.', key: 'Doctor', width: 20},
-  {header: 'Specialistas', key: 'Specialist', width: 20},
-  {header: 'Priezastis', key: 'Reason', width: 20},
-  {header: 'Perdavimo laikas', key: 'PassTime', width: 20},
-  {header: 'Prieme', key: 'AcceptBy', width: 20}
-];
+  const workbook = new excel.Workbook();
+  const consultationSheet = workbook.addWorksheet('Konsultacijos');
+  const consiliumSheet = workbook.addWorksheet('Konsiliumas');
+  const reportsSheet = workbook.addWorksheet('Ataskaita');
+  
+  consultationSheet.columns = [
+    {header: 'Id', key: 'ID', width: 10},
+    {header: 'Data', key: 'Time', width: 20},
+    {header: 'Skyrius', key: 'Department', width: 20},
+    {header: 'Skubus ar planinis', key: 'Urgency', width: 20},
+    {header: 'Palatos nr.', key: 'Room', width: 20},
+    {header: 'Pacientas', key: 'Patient', width: 20},
+    {header: 'Kvieciantysis gyd.', key: 'Doctor', width: 20},
+    {header: 'Specialistas', key: 'Specialist', width: 20},
+    {header: 'Priezastis', key: 'Reason', width: 20},
+    {header: 'Perdavimo laikas', key: 'PassTime', width: 20},
+    {header: 'Prieme', key: 'AcceptBy', width: 20}
+  ];
 
-app.get('/consultation/report', (req, res) => {
+  consiliumSheet.columns = [
+    {header: 'Id', key: 'ID', width: 10},
+    {header: 'Data', key: 'Time', width: 20},
+    {header: 'Skyrius', key: 'Department', width: 20},
+    {header: 'Palatos nr.', key: 'Room', width: 20},
+    {header: 'Pacientas', key: 'Patient', width: 20},
+    {header: 'Kvieciantysis gyd.', key: 'Doctor', width: 20},
+    {header: 'Specialistas', key: 'Specialist', width: 20},
+    {header: 'Priezastis', key: 'Reason', width: 20},
+    {header: 'Perdavimo laikas', key: 'PassTime', width: 20},
+    {header: 'Prieme', key: 'AcceptBy', width: 20}
+  ];
+
+  reportsSheet.columns = [
+      {header:'Palatos', key: 'Room',width: 20},
+      {header:'Kartai', key: 'rTimes',width: 10},
+      {header:'Pacientai', key: '',width: 20},
+      {header:'Kartai', key: '',width: 10},
+      {header:'Kvieciantysis gyd.', key: 'Doctor',width: 20},
+      {header:'Kartai', key: 'dTimes',width: 10},
+      {header:'Specialistai', key: '',width: 20},
+      {header:'Kartai', key: '',width: 10},
+      {header:'Priezastys', key: '',width: 20},
+      {header:'Kartai', key: '',width: 10},
+  ];
+    
   const {sorting,startingDate,startingTime,endingDate,endingTime} = req.query;
   const SELECT_CONSULT_WHERE = `SELECT * FROM Consultation WHERE Time BETWEEN '${databaseDateFormat(startingDate,startingTime)}' AND '${databaseDateFormat(endingDate,endingTime)}' ORDER BY ${sorting}`;
-   pool.query(SELECT_CONSULT_WHERE)
-  .then((rows) => {
+  const SELECT_CONSILIUM_WHERE = `SELECT * FROM Consilium WHERE Time BETWEEN '${databaseDateFormat(startingDate,startingTime)}' AND '${databaseDateFormat(endingDate,endingTime)}' ORDER BY ${sorting}`;
+  const COUNT_DB_VALUE = `SELECT Doctor, COUNT(*) dTimes FROM Consultation GROUP BY Doctor; SELECT Room, COUNT(*) rTimes FROM Consultation GROUP BY Room;`;
 
-    const tempFilePath = tempfile('.xlsx');
-    const excelRows = JSON.parse(JSON.stringify(rows));
+  pool.query(`${SELECT_CONSULT_WHERE}; ${SELECT_CONSILIUM_WHERE}; ${COUNT_DB_VALUE}`)
+  .then((results) => {
 
-    worksheet.addRows(excelRows);
-    workbook.xlsx.writeFile(tempFilePath).then(() => {
-      res.sendFile(tempFilePath, (err) => {
-        console.log('error downloading file: ' + err);
-      });
+    const consultExcelRows = JSON.parse(JSON.stringify(results[0]));
+    consultationSheet.addRows(consultExcelRows);
+    
+    const consilExcelRows = JSON.parse(JSON.stringify(results[1]));
+    consiliumSheet.addRows(consilExcelRows);
+
+    const reportsExcelRows = JSON.parse(JSON.stringify(results[2]));
+    const reportsExcelRows1 = JSON.parse(JSON.stringify(results[3]));
+    const excelRows = [...reportsExcelRows, ...reportsExcelRows1];
+    reportsSheet.addRows(excelRows);
+
+    res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    res.setHeader('Content-Disposition', 'attachment; filename=' + 'KonsultacijuAtaskaita.xlsx');
+
+    workbook.xlsx.write(res)
+    .then(() => {
+        res.status(200).end();
     });
+
   })
   .catch(err => {
-  console.log(err);
- })
-});  
+    console.log(err);
+  });
+}); 
 
 const databaseDateFormat = (date,time) => { 
   const monthShortNames = ["","Jan", "Feb", "Mar", "Apr", "May", "Jun",
